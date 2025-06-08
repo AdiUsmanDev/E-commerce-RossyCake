8fa689b7585a5d6e3b6087a43422bbf363cd1a2a
Hychtclyff <yudriqul20803@gmail.com>
2025-06-09 02:10:15 +0700

    refacotor :clean Code

--- Back-End/src/configs/app.js
+++ Back-End/src/configs/app.js
@@ -1,15 +1,20 @@
 import express, { json, urlencoded } from "express";
-import morgan from 'morgan';
-import apiV1 from '../routes/api/v1/index.js';
-import * as ErrorHandler from '../middlewares/errorHandler.js';
+import morgan from "morgan";
+import apiV1 from "../routes/api/v1/index.js";
+import cors from "cors";
+import * as ErrorHandler from "../middlewares/errorHandler.js";
 
 export const app = express();
 
-
+const corsOptions = {
+  origin: ["https://rossi-cake.vercel.app/", "http://localhost:5173"],
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 apiV1(app);
 
--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -1,27 +1,22 @@
-// const express = require("express");
-// const app = express();
-// const cartRoutes = require("./routes/cart.routes");
-// const authRoutes = require("./routes/auth.routes");
-// const accRoutes = require("./routes/accountRoute");
-// const adminRoutes = require("./routes/adminRoute");
-
-// app.use(express.json());
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/auth", authRoutes); //daftar admin dan user
-// app.use("/api/account",accRoutes);
-// app.use("/api/admin",adminRoutes); // menajemaen admin
-
-// app.listen(10000, () => console.log("Server running on http://localhost:3000"));
-
+// import "dotenv/config";
 import http from "http";
 import { app } from "./configs/app.js";
-
+import listEndpoints from "express-list-endpoints";
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || "localhost";
 
 const server = http.createServer(app);
 
 try {
+  if (process.env.NODE_ENV == "development") {
+    console.log("================== API - LIST =======================");
+    listEndpoints(app).forEach((route) => {
+      route.methods.forEach((method) => {
+        console.log(`Route => ${method} ${route.path}`);
+      });
+    });
+    console.log("================== API - LIST =======================\n");
+  }
   server.listen(port, () => {
     console.log(`🚀 Server is on ${host}:${port}`);
   });
