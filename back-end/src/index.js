1442a130a763c43b4578464bccf12119b6f1770c
Hychtclyff <yudriqul20803@gmail.com>
2025-06-10 10:34:09 +0700

    merge server

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,1 +1,25 @@
-    
+// import "dotenv/config";
+import http from "http";
+import { app } from "./configs/app.js";
+import listEndpoints from "express-list-endpoints";
+const port = process.env.PORT || 8000;
+const host = process.env.HOST || "localhost";
+
+const server = http.createServer(app);
+
+try {
+  if (process.env.NODE_ENV == "development") {
+    console.log("================== API - LIST =======================");
+    listEndpoints(app).forEach((route) => {
+      route.methods.forEach((method) => {
+        console.log(`Route => ${method} ${route.path}`);
+      });
+    });
+    console.log("================== API - LIST =======================\n");
+  }
+  server.listen(port, () => {
+    console.log(`🚀 Server is on ${host}:${port}`);
+  });
+} catch (error) {
+  console.log(`Error: ${error.message}`);
+}
